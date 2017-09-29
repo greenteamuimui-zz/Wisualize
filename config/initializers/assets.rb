d7c0ff1676e49275f5d32d3811c4020ea768e210
greenteamuimui <siuding0601@gmail.com>
2017-09-29 11:53:54 -0700

    Fix pre-compile issues

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,6 +8,9 @@ Rails.application.config.assets.version = '1.0'
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 Rails.application.config.assets.precompile += %w( bundle.js )
+config.assets.initialize_on_precompile = false
+
+
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
